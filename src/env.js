2ed481b43d6053e79907b8a228e4fc53da5585e7
John Lee <johnteee@gmail.com>
2018-05-30 16:50:05 +0800

    Add URLSearchParams for newer browsers

--- src/env.js
+++ src/env.js
@@ -312,12 +312,19 @@ class EnvUtilsDef {
     });
   }
   getParameterByName(name, url) {
-    if (!url) url = window.location.href;
+    if (!url) {url = window.location.href};
     name = name.replace(/[\[\]]/g, "\\$&");
+
+    if (URLSearchParams) {
+      // logMessage('with URLSearchParams');
+      var params = new URLSearchParams(window.location.search);
+      return params.has(name) ? params.get(name) : null;
+    }
+
     var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
         results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
+    if (!results) {return null};
+    if (!results[2]) {return ''};
     return decodeURIComponent(results[2].replace(/\+/g, " "));
   }
 }
